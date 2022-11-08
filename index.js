64f88b929c3bc472186591db1c92a7a7635d4c65
v_slechta <slechtav@outlook.com>
2022-11-08 19:48:21 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,102 @@
+//Digital Clock
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
+
+// Heading change based on time of day
+const headingChange = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  let heading = document.getElementById("heading");
+  if (hours < 12) {
+    heading.innerHTML = "Good Morning,";
+  } else if (hours < 18) {
+    heading.innerHTML = "Good Afternoon,";
+  } else {
+    heading.innerHTML = "Good Evening,";
+  }
+};
+headingChange();
+
+// Play music functionality
+document.addEventListener('DOMContentLoaded', () => {
+  myAudio = document.getElementById('myAudio');
+  myAudio.play();
+});
+
+// Not allowed by browser
+// window.onload = function() {
+//   document.getElementById("myAudio").play();
+// }
+
+const soundCloud = document.querySelector('.sound-cloud');
+const off = document.querySelector('#off');
+const on = document.querySelector('#on');
+const myAudio = document.querySelector('#myAudio');
+
+off.addEventListener('click', () => soundTrack('off'));
+on.addEventListener('click', () => soundTrack('on'));
+
+const soundTrack = (soundState) => {
+    if(soundState === 'off'){
+        on.style.display = 'block';
+        off.style.display = 'none';
+        soundCloud.style.color = "#08fdd8";
+        myAudio.play();
+    }
+    else if(soundState === 'on'){
+        on.style.display = 'none';
+        off.style.display = 'block';
+        soundCloud.style.color = "#f50057";
+        myAudio.pause();
+    }
+};
+
+
+// Navigation functionality
+const btnBars = document.querySelector('.bars');
+const btnTimes = document.querySelector('.times');
+const SideNav = document.querySelector('.aside');
+
+
+btnBars.addEventListener('click', () => myFunc('open'));
+btnTimes.addEventListener('click', () => myFunc('close'));
+
+const myFunc = (navCondition) => {
+    if(navCondition === 'open'){
+        SideNav.classList.add('show_nav');
+        btnTimes.style.display = "block";
+        btnBars.style.display = "none";
+    }
+    else if(navCondition === 'close'){
+            SideNav.classList.remove('show_nav');
+            btnTimes.style.display = "none";
+            btnBars.style.display = "block";
+    }
+};
